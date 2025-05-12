20b724b1cb3fb2ee55f4b91637df2a02413a2679
Angierc25 <carhuas2005@gmail.com>
2025-05-11 19:43:06 -0500

    fix(app): corregir error en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import { Routes } from 'react-router';
 
 function App() {
-  return 
-    <>  
-    </>
-  
+  return (
+    <div>  
+      <div>
+
+      </div>
+    </div>
+  );
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter as Router } from 'react-router';
-import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-const client = new QueryClient()
+import { BrowserRouter as Router } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
+const client = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <QueryClientProvider client={client}>
-    <Router>
-    <App />
-    </Router>
+        <Router>
+          <App />
+        </Router>
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
