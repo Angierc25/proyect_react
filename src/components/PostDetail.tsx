d365dd9c42e375365cbd36aa03fac9aefaf2fb20
Angierc25 <carhuas2005@gmail.com>
2025-05-17 18:53:43 -0500

    feat:Comentarios subidos

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import type { Post } from "./PostList";
 import { supabase } from "../supabase-client";
 import { LikeButton } from "./LikeButton";
+import { CommentSection } from "./CommentSection";
 
 
 interface Props {
@@ -47,6 +48,7 @@ const fetchPostById = async (id:number): Promise<Post> =>{
       </p>
 
        <LikeButton postId={postId} />
+       <CommentSection postId={postId}/>
       
     </div>
      );
